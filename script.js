c0fcfbe283d64f395a5a10cf296eef8c58bdc494
Kristaps Rezgalis <kristaps.rezgalis@gmail.com>
2026-03-06 17:41:24 +0200

    Add files via upload
    Minor updates in all files. Add new order form half done.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 'use strict'
 
-let createTransportData = function () {
+let ordersAll = [[1, 2, 3], [4, 5, 6]];
 
+let createTransportData = function () {
+    console.log(`Number of transport order: ${ordersAll.length}`);
+}
+
+let insertNewOrder = function () {
+    document.getElementById('enterOrder-container').style.display = 'none';
+    console.log(`Form button pressed!`);
 }
